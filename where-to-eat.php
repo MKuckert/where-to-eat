3130c67744a685bcab7127d6110b6b75655b2005
bspinner <boris.spinner@hs-osnabrueck.de>
2013-09-15 22:33:36 +0200

    fancy c64 style

--- where-to-eat.php
+++ where-to-eat.php
@@ -5,7 +5,7 @@ $seed=$date->getTimestamp();
 mt_srand($seed);
 
 $dinnerChances=array(
-	'Dönermann',
+	'Doenermann',
 	'Hallo Pizza',
 	'Subway',
 	'Chinamann',
@@ -14,20 +14,33 @@ $dinnerChances=array(
 $dice=mt_rand()%count($dinnerChances);
 $change=$dinnerChances[$dice];
 ?><!DOCTYPE HTML>
+<!-- c64 style from http://dl.dropboxusercontent.com/u/144888/wordpress/2011-03-16-commodore -->
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 		<title>Where dem food at?</title>
-		<style>
-body {
-	text-align: center;
-	margin-top: 10%;
-	font-size: 30pt;
-	font-family: sans-serif;
-}
-		</style>
+		<link rel="stylesheet" type="text/css" href="c64.css">
 	</head>
-	<body>
-		<em>Go to</em> <?php echo $change ?>
-	</body>
+
+	<body class="bg-type1">
+	<div class="bg-type2 basicscreen font-type1 text-space">
+	    <div class="txt-header">
+		<ul class="txt-header-list">
+		    <li>**** COMMODORE 64 BASIC V2 ****</li>
+		    <li>64K RAM SYSTEM 38911 BASIC BYTES FREE</li>
+		</ul>
+		READY.<br>
+		LOAD "*",8,1<br>
+		<br>
+		SEARCHING FOR *<br>
+		LOADING<br>
+		READY.<br>
+		RUN<br>
+		<br>
+		C64 SAYS: GOTO <?php echo $change; ?>
+		
+	    </div>
+        <img src="prompt.gif">
+	</div>
+    </body>
 </html>
